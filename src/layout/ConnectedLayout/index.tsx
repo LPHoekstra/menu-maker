55d0121e5394bdfa2b9a730b0c2167240f2717de
MalBourbie <MalBourbie@gmail.com>
2025-03-14 12:19:55 +0100

    refactor the Header, it is used on Home and ConnectedLayout

--- src/layout/ConnectedLayout/index.tsx
+++ src/layout/ConnectedLayout/index.tsx
@@ -1,54 +1,13 @@
 import { ReactElement } from "react";
-import { Link, Outlet, useLocation } from "react-router";
+import { Outlet } from "react-router";
 import m from "./index.module.scss";
-import logo from "../../assets/logo.png";
-
-interface navbarContent {
-    to: string
-    content: string
-}
-
-const navbarContent: Array<navbarContent> = [
-    {
-        to: "dashboard",
-        content: "Dashboard"
-    },
-    {
-        to: "",
-        content: "Mes menus"
-    },
-    {
-        to: "",
-        content: "Mon restaurant"
-    },
-    {
-        to: "",
-        content: "Mon compte"
-    }
-]
+import Header from "../Header";
 
 function ConnectedLayout(): ReactElement {
-    const location = useLocation()
-    const pathname: string = location.pathname.split("/")[1]
-
-
     return (
         <div className={m.layout}>
             <aside className={m.sidebar}>
-                <header className={m.header}>
-                    <img src={logo} alt="Menu Maker by Qwenta" />
-                    <nav className={m.navbar}>
-                        <ul>
-                            {navbarContent.map(list => (
-                                <li key={list.content}>
-                                    <Link to={list.to} className={`${m.navbar__link} ${pathname === list.to ? m.navbar__link_selected : ""}`}>
-                                        {list.content}
-                                    </Link>
-                                </li>
-                            ))}
-                        </ul>
-                    </nav>
-                </header>
+                <Header />
                 <footer className={m.footer}>
                     <ul>
                         <li>Se déconnecter</li>
--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,23 +1,78 @@
 import { ReactElement } from "react";
 import logo from "../../assets/logo.png";
 import m from "./index.module.scss";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
+
+interface navbarConnectedContent {
+    to: string
+    content: string
+}
+
+const navbarConnectedContent: Array<navbarConnectedContent> = [
+    {
+        to: "dashboard",
+        content: "Dashboard"
+    },
+    {
+        to: "",
+        content: "Mes menus"
+    },
+    {
+        to: "",
+        content: "Mon restaurant"
+    },
+    {
+        to: "",
+        content: "Mon compte"
+    }
+]
+
+interface navbarHomeContent {
+    to: string
+    content: string
+}
+
+const navbarHomeContent: Array<navbarHomeContent> = [
+    {
+        to: "",
+        content: "Exemples"
+    },
+    {
+        to: "",
+        content: "Tarifs"
+    },
+    {
+        to: "login",
+        content: "Se Connecter"
+    }
+]
 
 function Header(): ReactElement {
+    const location = useLocation()
+    const pathname: string = location.pathname.split("/")[1]
+
+    const navbarContent = pathname === "" ? navbarHomeContent : navbarConnectedContent
+
     return (
-        <header className={m.header}>
-            <img src={logo} alt="Menu Maker by Qwenta" className={m.header__logo} />
-            <nav>
-                <ul className={m.navbar}>
-                    <li>
-                        <a href="" className={m.navbar__text} data-text="Exemples">Exemples</a>
-                    </li>
-                    <li>
-                        <a href="" className={m.navbar__text} data-text="Tarifs">Tarifs</a>
-                    </li>
-                    <li>
-                        <Link to="login" className={m.navbar__text} data-text="Se Connecter">Se Connecter</Link>
-                    </li>
+        <header className={pathname === "" ? m.headerHome : m.headerConnected}>
+            <img src={logo} alt="Menu Maker by Qwenta" className={m.logo} />
+            <nav className={pathname === "" ? m.navbarHome : m.navbarConnected}>
+                <ul>
+                    {navbarContent.map(list => (
+                        <li key={list.content}>
+                            <Link to={list.to} className={
+                                pathname === "" ?
+                                    m.navbarHome__link
+                                    :
+                                    `${m.navbarConnected__link} ${pathname === list.to
+                                        ? m.navbarConnected__link_selected : ""
+                                    }`
+                            }
+                                data-text={list.content}>
+                                {list.content}
+                            </Link>
+                        </li>
+                    ))}
                 </ul>
             </nav>
         </header>
