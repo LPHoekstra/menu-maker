ade44a1f2a00a5a5dd41d59e944211a2190fe377
MalBourbie <MalBourbie@gmail.com>
2025-04-28 11:26:55 +0200

    Styles for category 3 and 4 for menu visualisation.

--- src/components/MenuVisualisation/index.tsx
+++ src/components/MenuVisualisation/index.tsx
@@ -12,17 +12,23 @@ function MenuVisualisation() {
                 <h2 className={m.menuWrapper__title}>MENU</h2>
                 <div className={m.categoriesWrapper}>
                     {Object.entries(menuData).map((category) => (
-                        <div className={m.categoryWrapper}>
+                        <div key={category[0]} className={m.categoryWrapper}>
                             <h3 className={m.categoryWrapper__title}>{category[0]}</h3>
-                            {category[1].map((dish) => (
-                                <div className={m.dishWrapper}>
-                                    <div className={m.dishWrapper__nameAndPriceWrapper}>
-                                        <h4 className={m.dishWrapper__name}>{dish.name}</h4>
-                                        <span className={m.dishWrapper__price}>{dish.price}€</span>
+                            <div className={m.dishesWrapper}>
+                                {category[1].map((dish) => (
+                                    <div key={dish.name}
+                                        className={`${m.dishWrapper} ${dish.description ? m.dishWrapper_withDescription : m.dishWrapper_withoutDescription}`}
+                                    >
+                                        <div className={m.dishWrapper__nameAndPriceWrapper}>
+                                            <h4 className={m.dishWrapper__name}>{dish.name}</h4>
+                                            <span className={m.dishWrapper__price}>{dish.price}€</span>
+                                        </div>
+                                        {dish.description &&
+                                            <p className={m.dishWrapper__desc}>{dish.description}</p>
+                                        }
                                     </div>
-                                    <p className={m.dishWrapper__desc}>{dish.description}</p>
-                                </div>
-                            ))}
+                                ))}
+                            </div>
                         </div>
                     ))}
                 </div>
