37b50b482ddefccf8e3ab7377b715fdcd7c424db
MalBourbie <MalBourbie@gmail.com>
2025-04-29 11:47:48 +0200

    ChangeMenuColor component. array availableFontFamily and availableColor for menu customization, used in inlineCSS.

--- src/components/MenuCustomization/index.tsx
+++ src/components/MenuCustomization/index.tsx
@@ -1,24 +1,39 @@
+import { useState } from "react"
+import MenuChangeColor from "../MenuChangeColor"
 import m from "./index.module.scss"
+import { availableFontFamily } from "../../utils/availableParamMenu"
 
 function MenuCustomization() {
+    const [isChangeColorOpen, setIsChangeColorOpen] = useState<boolean>(false)
+
+    const setChangeColorOpen = () => {
+        setIsChangeColorOpen(!isChangeColorOpen)
+    }
+
     return (
         <div>
             <article>
                 <h3 className={m.mainWrapper__title}>Choisissez une typographie</h3>
                 <div className={m.typoWrapper}>
-                    <button className={m.typoWrapper__btn}>Aa</button>
-                    <button className={m.typoWrapper__btn}>Aa</button>
-                    <button className={m.typoWrapper__btn}>Aa</button>
+                    {availableFontFamily.map((fontFamily) => (
+                        <button key={fontFamily}
+                            className={m.typoWrapper__btn}
+                            style={{ fontFamily: fontFamily }}
+                        >Aa</button>
+                    ))}
                 </div>
             </article>
-            <article>
+            <article className={m.changeColorWrapper}>
                 <h3 className={m.mainWrapper__title}>Choisissez une couleur</h3>
-                <button className={m.mainWrapper__colorBtn}>+</button>
+                <button className={m.changeColorWrapper__colorBtn} onClick={setChangeColorOpen}>+</button>
+                {isChangeColorOpen &&
+                    <MenuChangeColor setChangeColorOpen={setChangeColorOpen} />
+                }
             </article>
-            <article>
+            {/* <article>
                 <h3 className={m.mainWrapper__title}>Mise en page</h3>
                 <button></button>
-            </article>
+            </article> */}
         </div>
     )
 }
---
+++ src/utils/availableParamMenu.ts
@@ -0,0 +1,3 @@
+export const availableFontFamily: Array<string> = ['"Baskervville", serif', '"Rubik", sans-serif', '"Proza Libre", sans-serif']
+
+export const availableColor: Array<string> = ["#000", "#3678B9", "#ffd883"]
